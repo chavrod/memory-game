c3e933ce0b5555e3c66521abdc3ca6d3e177ca32
Dima Chavro <dchavro@gmail.com>
2023-03-02 14:16:00 +0300

    Modify app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import "./index.css";
+
+import Navbar from "./components/Navbar";
+import CardGrid from "./components/CardGrid";
+import { CARDS } from "./data/data.js";
+
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [currentGame, setCurrentGame] = useState(0);
+
+  const gameSet = [0, 1, 2, 3];
+
+  return (
+    <div>
+      <Navbar
+        currentGame={currentGame}
+        handleGameChange={(e) => setCurrentGame(e)}
+        gameSet={gameSet}
+      />
+      <CardGrid currentGame={currentGame} cards={CARDS} />
+    </div>
+  );
 }
 
 export default App;
