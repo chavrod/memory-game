092d3e8db5469f701abfbda0a62c1075d7e6f696
Dima Chavro <dchavro@gmail.com>
2023-03-02 15:27:27 +0300

    Add logic to keep track of score

--- src/App.js
+++ src/App.js
@@ -8,17 +8,45 @@ import { useState } from "react";
 
 function App() {
   const [currentGame, setCurrentGame] = useState(0);
+  const [selectedCardIds, setSelectedCardIds] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
-  const gameSet = [0, 1, 2, 3];
+  const handleCardSelection = (cardId) => {
+    setSelectedCardIds([...selectedCardIds, cardId]);
+    if (selectedCardIds.includes(cardId)) {
+      setScore(0);
+      setSelectedCardIds([]);
+      if (score > bestScore) setBestScore(score);
+    } else {
+      setScore((n) => n + 1);
+    }
+    console.log(selectedCardIds);
+  };
+
+  const handleGameChange = (game) => {
+    setCurrentGame(game);
+    setSelectedCardIds([]);
+    setScore(0);
+    setBestScore(0);
+  };
+
+  const gameSet = [0, 1];
 
   return (
     <div>
       <Navbar
         currentGame={currentGame}
-        handleGameChange={(e) => setCurrentGame(e)}
+        handleGameChange={handleGameChange}
         gameSet={gameSet}
+        score={score}
+        bestScore={bestScore}
       />
-      <CardGrid currentGame={currentGame} cards={CARDS} />
+      <CardGrid
+        currentGame={currentGame}
+        cards={CARDS}
+        handleCardSelection={handleCardSelection}
+      />
     </div>
   );
 }
