6ab429660ad94b480df8791c9853a506397b8d47
Dima Chavro <dchavro@gmail.com>
2023-03-02 14:15:20 +0300

    Add dropdown

---
+++ src/components/DropDown.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function DropDown({ currentGame, handleGameChange, gameSet }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setOpen(!open)}>Choose another game?</button>
+      {open ? (
+        <ul>
+          {gameSet.map((game) => (
+            <li key={game}>
+              <button
+                className={`hover:bg-gray-200 ${
+                  game === currentGame ? "bg-green-100" : ""
+                }`}
+              >
+                Set #{game + 1}
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
