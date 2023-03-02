55a71516b7bd9839f12e5f8928a300ae76b93e6c
Dima Chavro <dchavro@gmail.com>
2023-03-02 14:15:48 +0300

    Add CardGrid component

---
+++ src/components/CardGrid.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./Card";
+
+export default function CardGrid({ cards, currentGame }) {
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const shuffledCards = shuffle(cards[currentGame]);
+
+  return (
+    <div>
+      {shuffledCards.map((card) => (
+        <Card key={card.id} name={card.wordPair} img={card.img} />
+      ))}
+    </div>
+  );
+}
