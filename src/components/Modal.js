001f3f712acf8b309202779d96f3a8cbfa68df71
Dima Chavro <dchavro@gmail.com>
2023-03-02 16:04:01 +0300

    Add pop-up modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,25 @@
+export default function Modal({ handleGameChange }) {
+  return (
+    <div className="top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+      <div className="relative w-full h-full max-w-2xl md:h-auto">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Well done!! Keep playing...
+            </h3>
+          </div>
+          <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <button
+              onClick={(e) => handleGameChange(1, e)}
+              value={1}
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
