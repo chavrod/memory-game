7c0a52e361117360a8621ba6f96a9d882bcd6f14
Dima Chavro <dchavro@gmail.com>
2023-03-02 14:14:40 +0300

    Add data to the app

---
+++ src/data/data.js
@@ -0,0 +1,170 @@
+export const CARDS = [
+  [
+    {
+      id: 1,
+      wordPair: "Slow - Fast",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Slow-Fast-310x310.png>",
+    },
+    {
+      id: 2,
+      wordPair: "Straight - Curly",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Straight-Curly-310x310.png>",
+    },
+    {
+      id: 3,
+      wordPair: "Light - Heavy",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Light-Heavy-310x310.png>",
+    },
+    {
+      id: 4,
+      wordPair: "Loose - Tight",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Loose-Tight-310x310.png>",
+    },
+    {
+      id: 5,
+      wordPair: "Beautiful - Ugly",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Beautiful-Ugly-310x310.png>",
+    },
+    {
+      id: 6,
+      wordPair: "Big - Small",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Big-Small-310x310.png>",
+    },
+    {
+      id: 7,
+      wordPair: "Strong - Weak",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Strong-Weak-310x310.png>",
+    },
+    {
+      id: 8,
+      wordPair: "Low - High",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Low-High-310x310.png>",
+    },
+  ],
+  [
+    {
+      id: 9,
+      wordPair: "Poor - Wealthy",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Poor-Wealthy-310x310.png>",
+    },
+    {
+      id: 10,
+      wordPair: "Short - Tall",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Short-Tall-310x310.png>",
+    },
+    {
+      id: 11,
+      wordPair: "Thin - Fat",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Thin-Fat-310x310.png>",
+    },
+    {
+      id: 12,
+      wordPair: "Insane - Sane",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Insane-Sane-310x310.png>",
+    },
+    {
+      id: 13,
+      wordPair: "Bad - Good",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Bad-Good-310x310.png>",
+    },
+    {
+      id: 14,
+      wordPair: "Deep - Shallow",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Deep-Shallow-310x310.png>",
+    },
+    {
+      id: 15,
+      wordPair: "Dark - Light",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Dark-Light-310x310.png>",
+    },
+    {
+      id: 16,
+      wordPair: "Lazy - Hard-working",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Lazy-Hardworking-310x310.png>",
+    },
+  ],
+  [
+    {
+      id: 17,
+      wordPair: "Brave - Cowardly",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Brave-Cowardly-310x310.png>",
+    },
+    {
+      id: 18,
+      wordPair: "Cheap - Expensive",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/cheap-expensive-310x310.png>",
+    },
+    {
+      id: 19,
+      wordPair: "Distant - Near",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/near-1-310x310.png>",
+    },
+    {
+      id: 20,
+      wordPair: "Modern - Ancient",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/modern-ancient-310x310.png>",
+    },
+    {
+      id: 21,
+      wordPair: "Delicious - Awful",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/delicious-awful-310x310.png>",
+    },
+    {
+      id: 22,
+      wordPair: "Wide - Narrow",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/wide-narrow-310x310.png>",
+    },
+    {
+      id: 23,
+      wordPair: "Healthy - Sick",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/Healthy-Sick-310x310.png>",
+    },
+    {
+      id: 24,
+      wordPair: "Careful - Careless",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/careful-carelessness-310x310.png>",
+    },
+  ],
+  [
+    {
+      id: 25,
+      wordPair: "Pessimistic - Optimistic",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/pessimistic-optimistic-310x310.png>",
+    },
+    {
+      id: 26,
+      wordPair: "Tidy - Messy",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/tidy-messy-1-310x310.png>",
+    },
+    {
+      id: 27,
+      wordPair: "Patient - Impatient",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/patient-impatient-310x310.png>",
+    },
+    {
+      id: 28,
+      wordPair: "Friendly - Unfriendly",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/friendly-unfriendly-310x310.png>",
+    },
+    {
+      id: 29,
+      wordPair: "Cold - Hot",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/cold-hot-310x310.png>",
+    },
+    {
+      id: 30,
+      wordPair: "Dishonest - Honest",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/dishonest-honest-310x310.png>",
+    },
+    {
+      id: 31,
+      wordPair: "Happy - Unhappy",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/happy-unhappy-310x310.png>",
+    },
+    {
+      id: 32,
+      wordPair: "Selfish - Generous",
+      img: "<https://7esl.com/wp-content/uploads/2018/03/selfish-generous-310x310.png>",
+    },
+  ],
+];
